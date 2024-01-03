d7c03c4d6ae218335131015108d30563ce2862ea
lucashenrique22 <lh834392@gmail.com>
2024-01-03 13:33:26 -0300

    finaliza controller marca

--- app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -2,25 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
 use App\Models\Marca;
 use Illuminate\Http\Request;
-use Ramsey\Uuid\Type\Integer;
 
 class MarcaController extends Controller
 {
+    public function __construct(Marca $marca) {
+        $this->marca = $marca;
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-
-    public function __construct(Marca $marca)
-    {
-        $this->marca = $marca;
-    }
     public function index()
     {
-        //
+        //$marcas = Marca::all();
         $marcas = $this->marca->all();
         return response()->json($marcas, 200);
     }
@@ -43,20 +41,17 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        //
         $request->validate($this->marca->rules(), $this->marca->feedback());
 
-
         $imagem = $request->file('imagem');
-        $imagem_urn = $imagem->store('imagens','public');
-
+        $imagem_urn = $imagem->store('imagens', 'public');
 
         $marca = $this->marca->create([
             'nome' => $request->nome,
             'imagem' => $imagem_urn
-            ]
-        );
-       return response()->json($marca, 201);
+        ]);
+
+        return response()->json($marca, 201);
     }
 
     /**
@@ -67,14 +62,12 @@ class MarcaController extends Controller
      */
     public function show($id)
     {
-        //
         $marca = $this->marca->find($id);
-        if($marca === null)
-        {
-            return response()->json(['erro' => 'Recurso pesquisado nao existe'], 404);
+        if($marca === null) {
+            return response()->json(['erro' => 'Recurso pesquisado não existe'], 404) ;
         }
-        return response()->json($marca, 200);
 
+        return response()->json($marca, 200);
     }
 
     /**
@@ -97,43 +90,44 @@ class MarcaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
         $marca = $this->marca->find($id);
 
-        //dd($request->nome);
-        dd($request->file('imagem'));
-
-        if($marca === null)
-        {
-            return response()->json(['erro' => 'Não é possível realizar a edição. Recurso inexistente'], 404);
+        if($marca === null) {
+            return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'], 404);
         }
 
-        if($request->method() === 'PATCH')
-        {
-          $regrasDinamicas = array();
+        if($request->method() === 'PATCH') {
 
-          //percorrendo as regras definidas no model
-          foreach ($marca->rules() as $input => $regra)
-              {
-                    //coleta apenas as regras aplicaveis ao parametro do request
-                    if(array_key_exists($input, $request->all()) )
-                    {
-                        $regrasDinamicas[$input] = $regra;
-                    }
-              }
-          $request->validate($regrasDinamicas , $this->marca->feedback());
+            $regrasDinamicas = array();
+
+            //percorrendo todas as regras definidas no Model
+            foreach($marca->rules() as $input => $regra) {
+
+                //coletar apenas as regras aplicáveis aos parâmetros parciais da requisição PATCH
+                if(array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+            $request->validate($regrasDinamicas, $marca->feedback());
+
+        } else {
+            $request->validate($marca->rules(), $marca->feedback());
         }
-        else{
-            $request->validate($this->marca->rules(), $this->marca->feedback());
+
+        //remove o arquivo antigo caso um novo arquivo tenha sido enviado no request
+        if($request->file('imagem')) {
+            Storage::disk('public')->delete($marca->imagem);
         }
 
         $imagem = $request->file('imagem');
-        $imagem_urn = $imagem->store('imagens','public');
+        $imagem_urn = $imagem->store('imagens', 'public');
 
         $marca->update([
             'nome' => $request->nome,
             'imagem' => $imagem_urn
         ]);
+
         return response()->json($marca, 200);
     }
 
@@ -145,13 +139,17 @@ class MarcaController extends Controller
      */
     public function destroy($id)
     {
-        //
         $marca = $this->marca->find($id);
-        if($marca === null)
-        {
-            return response()->json(['erro' => 'Não é possível realizar a exclusão. Recurso inexistente'], 404);
+
+        if($marca === null) {
+            return response()->json(['erro' => 'Impossível realizar a exclusão. O recurso solicitado não existe'], 404);
         }
+
+        //remove o arquivo antigo
+        Storage::disk('public')->delete($marca->imagem);
+
         $marca->delete();
         return response()->json(['msg' => 'A marca foi removida com sucesso!'], 200);
+
     }
 }
